b9d7b629c9c98c830517a9a671a3f61d41bc0f4f
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-08-12 14:39:38 +1000

    added options to add and remove comments

--- client/src/components/post/Post.js
+++ client/src/components/post/Post.js
@@ -5,11 +5,13 @@ import Spinner from '../layout/Spinner';
 import PostItem from '../posts/PostItem';
 import { getPost } from '../../actions/post';
 import { Link } from 'react-router-dom';
+import CommentForm from './CommentForm';
+import CommentItem from './CommentItem';
 
 const Post = ({ getPost, post: { post, loading }, match }) => {
   useEffect(() => {
     getPost(match.params.id);
-  }, [getPost]);
+  }, [getPost, match.params.id]);
 
   return loading || post === null ? (
     <Spinner />
@@ -19,6 +21,12 @@ const Post = ({ getPost, post: { post, loading }, match }) => {
         Go Back
       </Link>
       <PostItem post={post} showActions={false} />
+      <CommentForm postId={post._id} />
+      <div className='comments'>
+        {post.comments.map(comment => (
+          <CommentItem key={comment._id} comment={comment} postId={post._id} />
+        ))}
+      </div>
     </Fragment>
   );
 };
